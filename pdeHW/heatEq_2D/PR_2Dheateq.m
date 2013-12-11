cf055f8fd51eb4590dedb6858dcf274286af5cc6
Atmosphere <r01543084@ntu.edu.tw>
2013-12-12 04:39:41 +0800

    PR終於寫好了，有時間改寫成沒有for loop的..OTZ

--- pdeHW/heatEq_2D/PR_2Dheateq.m
+++ pdeHW/heatEq_2D/PR_2Dheateq.m
@@ -6,17 +6,17 @@ clear all;close all;clc;
 %時間空間、離散
 dx = 0.5;
 dy = 0.5;
-dt = 0.001;
+dt = 0.01;
 x0 = 0;
 xEnd = 10;
 y0 = 0;
 yEnd = 10;
 tEnd = 10;
-k = 50;%heat coef.
+k = 30;%heat coef.
 x = x0:dx:xEnd;
 y = y0:dy:yEnd;
 t = 0:dt:tEnd;
-u = zeros(length(x),length(y));%initial condition
+u = ones(length(x),length(y));%initial condition
 rx = dt*k/dx^2
 ry = dt*k/dy^2
 %bc
@@ -27,18 +27,18 @@ u(:,1)   = 0;%x=0溫度
 u(:,end) = 10;%x=xEnd溫度
 %%
 %x direction Thomas Algorithm solver
-ajx = ones(1,length(u(1,:))-3)*(rx);
-bjx = ones(1,length(u(1,:))-3)*(rx);
-djx = ones(1,length(u(1,:))-2)*(1+2*rx);
+ajx = ones(1,length(u(1,:))-3)*(-rx/2);
+bjx = ones(1,length(u(1,:))-3)*(-rx/2);
+djx = ones(1,length(u(1,:))-2)*(1+rx);
 
 for i = 2:length(djx)
     djx(i) = djx(i)-bjx(i-1)/djx(i-1)*ajx(i-1);%get new dj
 end
 
 %y direction Thomas Algorithm solver
-ajy = ones(1,length(u(1,:))-3)*(ry);
-bjy = ones(1,length(u(1,:))-3)*(ry);
-djy = ones(1,length(u(1,:))-2)*(1+2*ry);
+ajy = ones(1,length(u(1,:))-3)*(-ry/2);
+bjy = ones(1,length(u(1,:))-3)*(-ry/2);
+djy = ones(1,length(u(1,:))-2)*(1+ry);
 
 for i = 2:length(djy)
     djy(i) = djy(i)-bjy(i-1)/djy(i-1)*ajy(i-1);%get new dj
@@ -47,9 +47,40 @@ end
 %main loop
 for n = 1:length(t)
     
-    up = circshift(u,[0 -1]); %ui,j+1
-    upp = circshift(u,[0 -2]);%ui,j+2
+
+    %step one
+    %x direction
+    for i = 2:length(x)-1
+        uTx = u(i,2:end-1);
+        cjx = uTx;
+        cjx(1) = cjx(1)+rx/2*u(i,1);
+        cjx(end) = cjx(end)+rx/2*u(i,end);
+        %===============solving loop==========
+        
+        u(i,2:end-1) = thomas(ajx,bjx,cjx,djx,uTx);
+    end
     
+    %y direction
+    for j = 2:length(y)-1
+        u(2:end-1,j) = ry/2*(u(2:end-1,j+1)-2*u(2:end-1,j)+u(2:end-1,j-1))+u(2:end-1,j);
+    end
+    
+    %step two
+    %x direction    
+    for i = 2:length(x)-1
+        u(i,2:end-1) = rx/2*(u(i+1,2:end-1)-2*u(i,2:end-1)+u(i-1,2:end-1))+u(i,2:end-1);
+    end
+    
+	%x direction
+    for j = 2:length(y)-1
+        uTy = u(2:end-1,j);
+        cjy = uTy;
+        cjy(1) = cjy(1)+ry/2*u(1,j);
+        cjy(end) = cjy(end)+ry/2*u(end,j);
+        %===============solving loop==========
+        
+        u(2:end-1,j) = thomas(ajy,bjy,cjy,djy,uTy);
+    end 
 	[C h]=contourf(u);
     clabel(C,h)
     caxis([0 20])
