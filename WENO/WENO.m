321d359ae6d3dea8d1ebb914eabc273e1908f352
Atmosphere <r01543084@ntu.edu.tw>
2013-12-13 10:49:00 +0800

    play with coef

--- WENO/WENO.m
+++ WENO/WENO.m
@@ -10,18 +10,18 @@
 clear all;close all;clc;
 x0   = -2*pi        ;%X初始位置
 xEnd = 2*pi         ;%X結束位置
-dx   = 0.5         ;%每dx切一格
-tEnd = 3            ;%從0開始計算tEnd秒 
-CFL  = 0.01          ;%CFL number
-a    = 10            ;%constant
+dx   = 0.1          ;%每dx切一格
+tEnd = 10           ;%從0開始計算tEnd秒 
+CFL  = 0.8          ;%CFL number
+a    = 1            ;%constant
 dt   = CFL*dx/a     ;%每dt切一格
 %%
 x = x0 : dx : xEnd;%切空間網格
 t = 0  : dt : tEnd;%切時間網格
 %%
 %mean progream
-u = heaviside(x)-heaviside(x-3);%initial condition
-%u = sin(x);
+%u = heaviside(x)-heaviside(x-3);%initial condition
+u = sin(x);
 for j = 1:length(t)
     F=[];%淨空F
     dF=[];%same
@@ -39,11 +39,11 @@ for j = 1:length(t)
     f = a*u_weno;%flux term
 	F = 0.5*((f(2,:)+f(1,:))-apha*(u_weno(2,:)-u_weno(1,:)));%LF flux
 	u_next = u - ( F(2:end)-F(1:end-1) )/dx*dt;%mean equation
-    uu = heaviside(x-a*dt*j)-heaviside(x-3-a*dt*j);
-    %uu = sin(x-a*dt*j);
+    %uu = heaviside(x-a*dt*j)-heaviside(x-3-a*dt*j);
+    uu = sin(x-a*dt*j);
     u_next(1)=u(end);%bc
     u = u_next;%renew u
-    plot(x,u,'.',x,uu,'--')
+    plot(x,u,'.',x,uu,'O')
     axis([x0-1, xEnd+1, min(uu)-0.5, max(uu)+0.5])
     xlabel('x');%水平座標名稱
     ylabel('u(t)');%垂直座標名稱
--- WENO/WENObyRK4th/WENObyrk3.m
+++ WENO/WENObyRK4th/WENObyrk3.m
@@ -8,11 +8,11 @@
 %%
 %輸入條件
 clear all;close all;clc;
-x0   = -3*pi        ;%X初始位置
-xEnd = 3*pi         ;%X結束位置
-dx   = 0.05          ;%每dx切一格
+x0   = -2*pi        ;%X初始位置
+xEnd = 2*pi         ;%X結束位置
+dx   = 0.1          ;%每dx切一格
 tEnd = 10           ;%從0開始計算tEnd秒 
-CFL  = 0.7          ;%CFL number
+CFL  = 0.8          ;%CFL number
 a    = 1            ;%constant
 dt   = CFL*dx/a     ;%每dt切一格
 %%
@@ -23,8 +23,8 @@ t = 0  : dt : tEnd;%切時間網格
 type = 1;%(1)Linear Advection, (2)Burgers' equation
 %%
 %mean progream
-u = exp(-(x+4).^2)+heaviside(x)-heaviside(x-3);%initial condition
-%u = sin(x);
+%u = exp(-(x+4).^2)+heaviside(x)-heaviside(x-3);%initial condition
+u = sin(x);
 for j = 1:length(t)
     F=[];%淨空F
     dF=[];%same
@@ -37,10 +37,11 @@ for j = 1:length(t)
     k2 = ( (-LF_flux(type,a,weno3(u+dt*k1/2)))         /dx );
     k3 = ( (-LF_flux(type,a,weno3(u+(k1+k2)*(dt/2)/2)))/dx );
 	u = u + 1/6*(k1+k2+4*k3)*dt;%mean equation
+    u(1) = (u(end)+u(1))/2;%periodic bc
     
-    uu = exp(-(x+4-a*dt*j).^2)+heaviside(x-a*dt*j)-heaviside(x-3-a*dt*j);
-    %uu = sin(x-a*dt*j);
-    plot(x,u,'.',x,uu,'--')
+    %uu = exp(-(x+4-a*dt*j).^2)+heaviside(x-a*dt*j)-heaviside(x-3-a*dt*j);
+    uu = sin(x-a*dt*j);
+    plot(x,u,'.',x,uu,'O')
     axis([x0-1, xEnd+1, min(uu)-0.5, max(uu)+0.5])
     xlabel('x');%水平座標名稱
     ylabel('u(t)');%垂直座標名稱
