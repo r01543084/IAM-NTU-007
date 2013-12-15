fb23411dad24f46b8c33e68af3c78598d303b980
Atmosphere <r01543084@ntu.edu.tw>
2013-12-15 16:23:49 +0800

    2D weno
    first time use weno in 2D but still use for loop

--- 2DUpwind/UPwind.m
+++ 2DUpwind/UPwind.m
@@ -11,11 +11,11 @@ dx = 0.3;
 y0 = -3*pi;
 yEnd = 3*pi;
 dy = 0.3;
-tEnd = 3;
+tEnd = 30;
 u = 10;%x方向速度
 v = 10;%y方向速度
-cflx = .1;
-cfly = .1;
+cflx = .9;
+cfly = .9;
 dt = min(cflx*dx/u,cfly*dy/v);
 cflx = dt*u/dx
 cfly = dt*v/dy
@@ -30,7 +30,7 @@ t = 0:dt:tEnd;
 [X,Y] = meshgrid(x,y);
 %U = heaviside(X)-heaviside(X-2)+heaviside(Y)-heaviside(Y-2);
 %U = heaviside(X)-heaviside(X-2)+sin(Y);
-U = exp(-*(X).^2-(Y).^2);
+U = exp(-(X).^2-(Y).^2);
 for i = 1:length(t)
     U_next = zeros(length(x),length(y));
     U_next(:,2:end) = U(:,2:end)-cflx*(U(:,2:end)-U(:,1:end-1));%x
@@ -40,5 +40,6 @@ for i = 1:length(t)
     U_next(1,:) = U(end,:);%y
     U = U_next;
     mesh(x,y,U)
+    grid on
     pause(dt)
 end
---
+++ WENO2D/LF_flux.m
@@ -0,0 +1,14 @@
+%flux calculator
+function [LF] = LF_flux(type,a,u)
+switch type
+    case 1
+        flux = @(w) a*w;
+        apha  = max(abs(a));%·L¤Àflux
+    case 2
+        flux = @(w) w.*w;
+        apha  = max(max(abs(u)));%·L¤Àflux
+end
+    f = flux(u);
+    
+	F = 0.5*((f(2,:)+f(1,:))-apha*(u(2,:)-u(1,:)));%LF flux
+    LF = ( F(2:end)-F(1:end-1) );
