cc303bd6ef90a0de9a563846805aa00baa2cce6e
Atmosphere <r01543084@ntu.edu.tw>
2014-02-22 15:51:34 +0800

    plot and saving!
    DON’T GO TO TEC ANYMORE!!(cross finger)

---
+++ BGK/Copy_of_2d/ID_name.m
@@ -0,0 +1,32 @@
+function [ID, IDn] = ID_name(name,theta,nx,ny,RK_stages,tau,IC_case)
+%% ID name generator 
+% Generates ID and IDn for an specific simulation using the controling 
+% parameters.
+
+%% Read Parameters
+% Statistic Used
+switch theta
+    case {-1}
+        statistic = 'BE';
+    case {0}
+        statistic = 'MB';
+    case {1}
+        statistic = 'FD';
+    otherwise
+        error('not a valid theta value')
+end
+% Elements used
+xelements  = num2str(nx);
+yelements  = num2str(ny);
+% RK stages
+RKs = num2str(RK_stages);
+% Relaxation time
+rtime = num2str(abs(log10(tau)));
+% Initial Condition Number
+ic = num2str(IC_case);
+% Tecplot format
+f = '.plt';
+
+%% Generate ID
+IDn = [name,'-',statistic,'-',xelements,'x',yelements,'RK',RKs,'rtime',rtime,'-','IC',ic,f];
+ID = [name,'-',statistic,'-',xelements,'x',yelements,'RK',RKs,'rtime',rtime,'-','IC',ic];
